0b878e466d5ac8e7be5515ff371572a8babd2a15
Lucas Esteves <lucas.esteves.rocha@gmail.com>
2022-08-24 15:51:06 +0200

    Canny algorithm

--- src/ridope_sp.c
+++ src/ridope_sp.c
@@ -40,13 +40,6 @@ uint8_t ridope_histogram(const uint8_t *img_in, size_t img_size, float *hist_out
 		hist_out[value] += 1;
 	}
 
-	/* Normalizing the histogram */
-	for(int i = 0; i <= hist_max; i++)
-	{
-		hist_out[i] = hist_out[i]/img_size;
-	}
-
-
 	return 0;
 }
 
@@ -59,7 +52,7 @@ uint8_t ridope_histogram(const uint8_t *img_in, size_t img_size, float *hist_out
  * @param width 	Width of the input image	
  * @return uint8_t  Return 0 if success
  */
-uint8_t ridope_otsu(const uint8_t *img_in, uint8_t *img_out, size_t height, size_t width)
+uint8_t ridope_otsu(const uint8_t *img_in, uint8_t *img_out, uint8_t *threshold, size_t height, size_t width)
 {
 	/* Checking input pointers */
 	if(img_in == NULL)
@@ -67,63 +60,60 @@ uint8_t ridope_otsu(const uint8_t *img_in, uint8_t *img_out, size_t height, size
 		return 1;
 	}
 
-	if(img_out == NULL)
+	if(img_out == NULL && threshold == NULL)
 	{
 		return 2;
 	}
 
+	if(threshold == NULL)
+	{
+		return 3;
+	}
+
 	/* Variable initialization */
 	int N = height*width;
-	uint16_t max_intensity = 255;
-	double w0, w1, u0, u1, max_var, class_var = 0;
-	uint16_t threshold;
+
+	*threshold = 0;
+	uint8_t max_intensity = 255;
+	double var_class, var_max, sum, sumB, q1, q2, u1, u2 = 0;
 	float histogram[max_intensity+1];
-	float cum_sum[max_intensity+1];
-	float mean[max_intensity+1];
 
 	/* Image histogram */
 	ridope_histogram(img_in, N, &histogram[0], max_intensity);
 
-	/* Auxiliary parameters */
-	cum_sum[0] = histogram[0];
-	mean[0] = 0;
-
-	for(int i = 1; i <= max_intensity; i++)
+	for (int i = 0; i <= max_intensity; i++)
 	{
-		cum_sum[i] = cum_sum[i-1] + histogram[i];
-		mean[i] = mean[i-1] + i*histogram[i];
+		sum += i*histogram[i];
 	}
 
-	/* Otsu's threshold method */
-	for(int i = 0; i <= max_intensity; i++)
+	for (int i = 0; i <= max_intensity; i++)
 	{
-		w0 = cum_sum[i];
-		w1 = 1 - cum_sum[i];
+		q1 += histogram[i];
 
-		u0 = mean[i]/cum_sum[i];
-		u1 = (mean[max_intensity] - mean[i])/(1 - cum_sum[i]);
+		if(q1 == 0) continue;
 
-		if(cum_sum[i] != 0.0 && cum_sum[i] != 1.0)
+		q2 = N - q1;
+
+		sumB += i*histogram[i];
+		u1 = sumB/q1;
+		u2 = (sum - sumB)/q2;
+
+		var_class = q1*q2*((u1 - u2)*(u1 - u2));
+
+		if (var_class > var_max)
 		{
-			class_var = ((mean[max_intensity]*w0 - mean[i])*(mean[max_intensity]*w0 - mean[i]))/(w0*w1);
-		}
-		else
-		{
-			class_var = 0;
+			*threshold = i;
+			var_max = var_class;
 		}
 
-
-		if(class_var > max_var)
-		{
-			max_var = class_var;
-			threshold = i;
-		}
 	}
 
-	/* Applying Threshold */
+	/* Applying Threshold if output image is not null */
+	if(img_out != NULL)
+	{
 	for(int i = 0; i < N; i++)
 	{
-		if (img_in[i] > threshold)
+				if (img_in[i] > *threshold)
 		{
 			img_out[i] = 255;
 		}
@@ -131,6 +121,7 @@ uint8_t ridope_otsu(const uint8_t *img_in, uint8_t *img_out, size_t height, size
 		{
 			img_out[i] = 0;
 		}
+	}
 	}
 
 	return 0;
@@ -284,9 +275,9 @@ uint8_t ridope_conv(const uint8_t *img_in, uint8_t *img_out, size_t height, size
 	double pixel_result;
 	double pixel_mask;
 
-	for(int x = 0; x < height; x++)
+	for(int x = 0; x < width; x++)
 	{
-		for(int y = 0; y < width; y++)
+		for(int y = 0; y < height; y++)
 		{
 			pixel_result = 0;
 
@@ -501,6 +492,339 @@ uint8_t ridope_get_mag_ang(const uint8_t *img_x_in, const uint8_t *img_y_in, uin
 			}
 		}
 	}
+
+	ridope_scaling(mag_out, height, width);
+
+	return 0;
+}
+
+/**
+ * @brief Searches and returns the max pixel value of the image
+ * 
+ * @param img_in 		Pointer to the input image	
+ * @param height 		Height of the input image
+ * @param width 		Width of the input image  
+ * @return uint8_t 		The max pixel value of the image input 
+ */
+uint8_t ridope_get_max(const uint8_t *img_in, size_t height, size_t width)
+{
+	/* Checking input pointers */
+	if(img_in == NULL)
+	{
+		return 0;
+	}
+
+	int max = 0;
+
+	for(int i = 0; i < height*width; i++)
+	{
+		if(img_in[i] > max) max = img_in[i];
+	}
+
+	return max;
+}
+
+/**
+ * @brief Scales the image between 0 and 255
+ * 
+ * @param img_in_out 	Pointer to the input/output image	
+ * @param height 		Height of the input image
+ * @param width 		Width of the input image  
+ * @return uint8_t 		Returns 0 if success  
+ */
+uint8_t ridope_scaling(uint8_t *img_in_out, size_t height, size_t width)
+{
+	/* Checking input pointers */
+	if(img_in_out == NULL)
+	{
+		return 1;
+	}
+
+	int max = ridope_get_max(img_in_out, height, width);
+
+	if (max == 0){
+		return 0;
+	}
+
+	for(int i = 0; i < height*width; i++)
+	{
+		img_in_out[i] = img_in_out[i] * 255/ max;
+	}
+	return 0;
+}
+
+/**
+ * @brief Checks if a index is out of bounds and corrects it to the limits
+ * 
+ * @param index 		The index to be checked
+ * @param limit_min 	The lower limit
+ * @param limit_max 	The upper limit
+ * @return uint8_t 		The corrected index
+ */
+uint8_t  get_updated_index(int index, uint8_t limit_min, uint8_t limit_max){
+
+	if(index < limit_min)
+	{
+		return limit_min;
+	}
+
+	if(index > limit_max)
+	{
+		return limit_max;
+	}
+
+	return index;
+}
+
+/**
+ * @brief Supresses the non-local-maximum pixels in the image
+ * 
+ * @param mag_in 		Pointer to the Sobel magnitude matrix
+ * @param ang_in 		Pointer to the Sobel angle matrix
+ * @param img_out 		Pointer to the output image
+ * @param height 		Height of the input image
+ * @param width 		Width of the input image  
+ * @return uint8_t 		Returns 0 if success
+ */
+uint8_t ridope_non_max_supression(uint8_t *mag_in, uint8_t *ang_in, uint8_t *img_out, size_t height, size_t width)
+{
+	/* Checking input pointers */
+	if(mag_in == NULL)
+	{
+		return 1;
+	}
+
+	if(ang_in == NULL)
+	{
+		return 2;
+	}
+
+	if(img_out == NULL)
+	{
+		return 3;
+	}
+
+	for(int x = 0; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+			int neighbor_x_1, neighbor_y_1;
+			int neighbor_x_2, neighbor_y_2;
+
+			if(ang_in[width * x + y] == 0)
+			{
+				neighbor_x_1 = x;
+				neighbor_y_1 = get_updated_index(y - 1, 0, height-1);
+				neighbor_x_2 = x;
+				neighbor_y_2 = get_updated_index(y + 1, 0, height-1);
+			}
+			else if(ang_in[width * x + y] == 45)
+			{
+				neighbor_x_1 = get_updated_index(x - 1, 0, width-1);
+				neighbor_y_1 = get_updated_index(y + 1, 0, height-1);
+				neighbor_x_2 = get_updated_index(x + 1, 0, width-1);
+				neighbor_y_2 = get_updated_index(y - 1, 0, height-1);
+			}
+			else if(ang_in[width * x + y] == 90)
+			{
+				neighbor_x_1 = get_updated_index(x - 1, 0, width-1);
+				neighbor_y_1 = y;
+				neighbor_x_2 = get_updated_index(x + 1, 0, width-1);
+				neighbor_y_2 = y;
+			}
+			else if(ang_in[width * x + y] == 135)
+			{
+				neighbor_x_1 = get_updated_index(x - 1, 0, width-1);
+				neighbor_y_1 = get_updated_index(y - 1, 0, height-1);
+				neighbor_x_2 = get_updated_index(x + 1, 0, width-1);
+				neighbor_y_2 = get_updated_index(y + 1, 0, height-1);
+			}
+
+			if((mag_in[width * x + y] >= mag_in[width * neighbor_x_1 + neighbor_y_1]) && (mag_in[width * x + y] >= mag_in[width * neighbor_x_2 + neighbor_y_2]))
+			{
+				img_out[width * x + y] = mag_in[width * x + y];
+			}
+			else
+			{
+				img_out[width * x + y] = 0;
+			}
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * @brief Performs the Hysteresis Thresholding
+ * 
+ * @param img_in_out 		Pointer to the input/output image
+ * @param high_threshold 	The upper limit threshold
+ * @param low_threshold 	The lower limit threshold
+ * @param height 			Height of the input image
+ * @param width 			Width of the input image  
+ * @return uint8_t 			Returns 0 if success
+ */
+uint8_t ridope_edge_tracking(uint8_t *img_in_out, uint8_t high_threshold, uint8_t low_threshold, size_t height, size_t width)
+{
+	/* Checking input pointers */
+	if(img_in_out == NULL)
+	{
+		return 1;
+	}
+
+	int strong_edges_size = 0;
+	int weak_edges_size = 0;
+
+	/* Counting strong and weak edges */
+	for(int x = 0; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+
+			if(img_in_out[width * x + y] >= high_threshold)
+			{
+				strong_edges_size++;
+			}
+			else if((img_in_out[width * x + y] >= low_threshold) && (img_in_out[width * x + y] < high_threshold))
+			{
+				weak_edges_size++;
+			}
+
+		}
+	}
+
+	/* Thresholding edges and saving indexes */
+	int * weak_edges_indexes = (int * ) malloc(2*weak_edges_size*sizeof(int));
+
+	int weak_i = 0;
+
+	for(int x = 0; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+
+			if(img_in_out[width * x + y] < low_threshold)
+			{
+				img_in_out[width * x + y] = 0;
+			}
+			else if(img_in_out[width * x + y] >= high_threshold)
+			{
+				img_in_out[width * x + y] = 255;
+			}
+			else
+			{
+				weak_edges_indexes[weak_i++] = x;
+				weak_edges_indexes[weak_i++] = y;
+			}
+
+		}
+	}
+
+	/* Finding weak edges connected to strong edges (8-connected neighborhood) */
+	for(int i = 0; i < weak_edges_size; i += 2)
+	{
+		uint8_t is_connected = 0;
+		int weak_edge_x = weak_edges_indexes[i];
+		int weak_edge_y = weak_edges_indexes[i+1];
+
+		for(int x = -1; x <1; x++)
+		{
+			for(int y = -1; y < 1; y++)
+			{
+				int img_x = get_updated_index(weak_edge_x-x, 0, width);
+				int img_y = get_updated_index(weak_edge_y-y, 0, height);
+
+				if(img_in_out[width * img_x + img_y] == 255)
+				{
+					is_connected = 1;
+				}
+			}
+		}
+
+		if(is_connected)
+		{
+			img_in_out[width * weak_edge_x + weak_edge_y] = 255;
+			weak_edges_indexes[i] = -1;
+			weak_edges_indexes[i+1] = -1;
+		}
+	}
+
+	/* Removing weak edges that are not connected */
+	for(int i = 0; i < weak_edges_size; i += 2)
+	{
+		int weak_edge_x = weak_edges_indexes[i];
+		int weak_edge_y = weak_edges_indexes[i+1];
+
+		if(weak_edge_x != -1 && weak_edge_y != -1)
+		{
+			img_in_out[width * weak_edge_x + weak_edge_y] = 0;
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * @brief Performs the Canny edge detection
+ * 
+ * @param img_in 			The pointer to the input image	
+ * @param img_out 			The pointer to the output image
+ * @param high_threshold 	The upper limit threshold
+ * @param low_threshold 	The lowzer limit threshold
+ * @param height 			Height of the input image
+ * @param width 			Width of the input image  
+ * @return uint8_t 			Returns 0 if success
+ */
+uint8_t ridope_canny(uint8_t *img_in, uint8_t *img_out, uint8_t high_threshold, uint8_t low_threshold, size_t height, size_t width)
+{
+	/* Checking input pointers */
+	if(img_in == NULL)
+	{
+		return 1;
+	}
+
+	if(img_out == NULL)
+	{
+		return 2;
+	}
+
+	uint8_t *g_x = (uint8_t *) malloc(height*width*sizeof(uint8_t));
+	uint8_t *g_y = (uint8_t *) malloc(height*width*sizeof(uint8_t));
+	uint8_t *ang = (uint8_t *) malloc(height*width*sizeof(uint8_t));
+
+	if(g_x == NULL)
+	{
+		return 3;
+	}
+
+	if(g_y == NULL)
+	{
+		return 4;
+	}
+
+	if(ang == NULL)
+	{
+		return 5;
+	}
+
+	uint8_t gaussian_ker_size = 5;
+	uint8_t sobel_ker_size = 3;
+	float sigma = 1.0;
+
+
+	ridope_gaussian_filter(img_in, img_out, height, width, gaussian_ker_size, sigma);
+	ridope_sobel_filter(img_out, g_x, g_y, height, width, sobel_ker_size);
+	ridope_get_mag_ang(g_x, g_y, img_out, ang, height, width);
+
+	free(g_y);
+
+	uint8_t *img_temp = g_x;
+	g_x = NULL;
+
+	ridope_non_max_supression(img_out, ang, img_temp, height, width);
+	ridope_edge_tracking(img_temp, high_threshold, low_threshold, height, width);
+
+	memcpy(img_out,img_temp,height*width);
 
 	return 0;
 }
